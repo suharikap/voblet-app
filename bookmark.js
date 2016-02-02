5e816a5971288ceab88186e7dfbbf81729f17062
suharikap <suharikap@gmail.com>
2016-02-02 11:02:08 +0530

    bookmark

---
+++ bookmark.js
@@ -0,0 +1,182 @@
+.page-content
+{
+	width: 100%;
+}
+.mdl-grid
+{
+	padding: 0;
+}
+.mdl-cell
+{
+   margin: 0;
+}
+.mdl-card
+{
+	height: 100%;
+	width: 100%;
+}
+.ddl-card
+{
+   width: 103%;
+}
+.card
+{
+	display: flex;
+	padding-left: 2%;
+}
+.left-pane-top-text
+{
+    font-weight: bold;
+    font-size: 30px;
+    text-align: center;
+    padding-top: 30px;
+    padding-bottom: 10px;
+}
+.bdl-card
+{
+    width: 192px;
+    height: 230px;
+    margin-top: 5%;
+    margin-left: 2%;
+}
+.card-inner
+{
+    height: 144px;
+    background-color: #fff;
+}
+.card-inner-top-text
+{
+	padding: 60px 85px;
+	font-weight: bold;
+}
+.card-inner-bottom
+{
+	display: flex;
+}
+.card-icon
+{
+	padding-left: 100px;
+	padding-top: 10px;
+
+}
+.card-inner-text
+{
+	font-weight: bold;
+	padding: 10px;
+}
+a
+{
+	color: #000;
+}
+.star-icon
+{
+	width: 24px;
+	padding-left: 15px;
+}
+.icon
+{
+	padding-left: 15px;
+}
+.left-pane-text
+{
+	padding-top: 5px;
+	padding-left: 15px;
+	font-weight: bold;
+	font-size: 15px;
+}
+
+.left-pane
+{
+	display: flex;
+}
+.button-left
+{
+	width: 150px;
+    margin-left: 30px;
+}
+.nav-item
+{
+	display: inline-block;
+	
+	text-decoration: none;
+}
+.nav-content
+{
+	position: absolute;
+	overflow: hidden;
+    max-height: 0;
+    background-color: #fff;
+    color: #000;
+}
+.nav-content a
+{
+	color: #fff;
+	text-decoration: none;
+}
+.nav-content a: hover
+{
+	text-decoration: underline;
+}
+
+.nav-sub
+{
+  padding: 10px;
+  width: 60px;
+}
+.nav-sub ul
+{
+padding: 0;
+margin: 0;
+list-style-type: none;
+}
+.nav-item:focus
+{
+	background-color: #444;
+}
+.nav-item:focus ~ .nav-content
+{
+	max-height: 400px;
+	-webkit-transition:max-height 0.4s ease-in;
+	-moz-transition:max-height 0.4s ease-in;
+	transition:max-height 0.4s ease-in;
+
+}
+.search
+{
+	display: flex;
+	padding-left: 20px;
+}
+.top-right
+{
+    padding-left: 33%;
+    padding-top: 20px;
+    
+}
+.top-right-button
+{
+	width: 25%;
+	margin-bottom: 15px;
+	margin-left: 5px;
+}
+.top-right-icon
+{
+    padding-right: 20px;
+}
+.search-box
+{
+  padding: 2px;
+  width: 100%;
+  height: 37px;
+
+}
+.search-bx
+{
+	width: 45%;
+	padding: 20px;
+	display: flex;
+}
+.project-img
+{
+	height: 100%;
+	width: 100%;
+}
